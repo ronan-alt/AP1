5bb7032e4a43c443d15ee98e42065c9a41c46f83
elyosssss <emechri.ledantec@gmail.com>
2025-10-14 10:32:42 +0200

    el

--- MauiApp1/Vues/Register.xaml.cs
+++ MauiApp1/Vues/Register.xaml.cs
@@ -1,17 +1,43 @@
-using MauiApp1;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Maui.Controls;
+using System;
 
 namespace AP1.Vues;
 
 public partial class Register : ContentPage
 {
-	public Register()
-	{
-		InitializeComponent();
-
-
-    }
-    private void OnRegisterClicked(object sender, EventArgs e)
+    public Register()
     {
-        DisplayAlert("Inscription", "Tu as cliqué sur le bouton S'inscrire !", "OK");
+        InitializeComponent();
+
     }
+
+    private async void OnRegisterClicked(object sender, EventArgs e)
+    {
+        string name = NameEntry.Text;
+        string email = EmailEntry.Text;
+        string password = PasswordEntry.Text;
+        string confirmPassword = ConfirmPasswordEntry.Text;
+
+        if (name == "" || email == "" || password == "" || confirmPassword == "")
+        {
+            await DisplayAlert("Erreur", "Tous les champs sont obligatoires.", "OK");
+            return;
+        }
+
+        if (password != confirmPassword)
+        {
+            await DisplayAlert("Erreur", "Les mots de passe ne correspondent pas.", "OK");
+            return;
+        }
+
+        await DisplayAlert("Succès", "Compte créé avec succès !", "OK");
+    }
+    private async void OnBackClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new LoginPage());
+    }
+
+
 }
+
