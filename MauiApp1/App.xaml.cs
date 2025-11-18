8c9c91ab31ff65a90e314bd9dae7a6013d5b9ef5
rnft <rnft78@gmail.com>
2025-11-18 13:40:55 +0100

    Update App.xaml.cs

--- MauiApp1/App.xaml.cs
+++ MauiApp1/App.xaml.cs
@@ -8,7 +8,7 @@ namespace MauiApp1
         public App()
         {
             InitializeComponent();
-            MainPage = new NavigationPage(new AccueilProfesseur());
+            MainPage = new NavigationPage(new LoginPage());
         }
 
     }
